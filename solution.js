4fe9e9583366eeb6387396076d1887f9fa3f7132
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-01-12 14:37:18 -0500

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,4 +1,4 @@
-Given the head of a linked list, remove the nth node from the end of the list and return its head.
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
 var removeNthFromEnd = function(head, n) {
         let dummy = new ListNode(0);
